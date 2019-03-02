b97552803b31540fcd69db60b22140ec37315e9a
nicospz <niko.ep@gmail.com>
2019-03-03 00:35:32 +0900

    Finished

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,25 @@
 class Book
 # write your code here
+    def title=t
+        dontCapitalize = ["and", "or", "in", "on", "the", "of", "a", "to", "an"]
+        split = t.split
+        capitalizedTitle = ""
+        first = true
+        split.each do |word|
+            if (!dontCapitalize.include? word)
+                word = word.capitalize
+            end
+            if (first || word == "i")
+                word = word.capitalize
+            end
+            capitalizedTitle += word + " "
+            first = false
+        end
+        capitalizedTitle[-1] = ""
+        @title = capitalizedTitle
+    end
+
+    def title
+        @title
+    end
 end

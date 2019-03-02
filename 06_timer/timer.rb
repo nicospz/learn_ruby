23f3ca7ff0d7473fc6b603e9a78eafa87b6db632
nicospz <niko.ep@gmail.com>
2019-03-03 01:49:31 +0900

    Finished

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,13 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    minutes = @seconds/60
+    hours = minutes/60
+    sprintf("%02d:%02d:%02d",hours,minutes%60,@seconds%60)
+  end
 end
